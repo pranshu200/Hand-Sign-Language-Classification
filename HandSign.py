4c88766b531de8b4fd572d2e5919177624a30126
pranshu200 <pranshuy2020@gmail.com>
2022-04-30 23:37:14 +0530

    Training and Testing of model

--- HandSign.py
+++ HandSign.py
@@ -20,11 +20,25 @@ print("----Folder Loaded----")
 # %%
 os.listdir()
 #%%
+#Loading the Dataset
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-training_path= 'C:/PRANSHU\PROJECTS/Hand-Sign-Language-Classification/NewDataset\Train-Dataset'
-validation_path= 'C:/PRANSHU\PROJECTS/Hand-Sign-Language-Classification/NewDataset\Validation-Dataset'
-train_datagen = ImageDataGenerator()
-val_datagen = ImageDataGenerator(rescale=1./255)
+training_path= 'C:/PRANSHU/PROJECTS/Hand-Sign-Language-Classification/DATASET/train'
+validation_path= 'C:/PRANSHU/PROJECTS/Hand-Sign-Language-Classification/DATASET/validation'
+train_datagen =ImageDataGenerator(
+        rescale=1.0 / 255.0, 
+        rotation_range=0,  
+        zoom_range = 0.15,  
+        width_shift_range=0.10,  
+        height_shift_range=0.10,  
+        horizontal_flip=False,  
+        vertical_flip=False) 
+val_datagen = ImageDataGenerator(rescale=1.0 / 255.0, 
+        rotation_range=0,  
+        zoom_range = 0.15,  
+        width_shift_range=0.10,  
+        height_shift_range=0.10,  
+        horizontal_flip=False,  
+        vertical_flip=False)
 training_set = train_datagen.flow_from_directory(training_path,target_size=(64,64),batch_size=10,class_mode='categorical')
 validation_set = val_datagen.flow_from_directory(validation_path,target_size=(64,64),batch_size=10,class_mode='categorical')
 print("Dataset Loaded")
@@ -43,10 +57,11 @@ plot_model(model, to_file='CNN_Layers.png')
 with tf.device('/GPU:0'):
     history = model.fit(
         training_set,
-        steps_per_epoch=3750//25,
-        epochs=50,
+        steps_per_epoch=14400//10,
+        epochs=100,
+        verbose=1,
         validation_data = validation_set,
-        validation_steps=600/25
+        validation_steps=3600//10
         #callbacks=[earlystopping]
     )
 #%%
@@ -58,6 +73,7 @@ plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['training_accuracy', 'validation_accuracy'])
 plt.show()
+plt.savefig('Accuracy.jpg')
 #%%
 #Loss
 
@@ -68,12 +84,13 @@ plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['training_loss', 'validation_loss'])
 plt.show()
+plt.savefig('Loss.jpg')
 #%%
 #Save Model
 m_json = model.to_json() 
-with open("CNN_model.json", "w") as json_file:  
+with open("HandSignModel1.json", "w") as json_file:  
     json_file.write(m_json)  
-model.save_weights("CNN_model_Weights.h5")
+model.save_weights("HandSignModel1_Weights.h5")
 # %%
 #Loading the Saved Model
 from tensorflow.keras.models import load_model
@@ -92,25 +109,22 @@ def process_image(path):
     img_tensor/=255.0
     return img_tensor
 #%%
-
-path1 = 'C:/PRANSHU/PROJECTS/Hand-Sign-Language-Classification/images.jpg'
+#Testing
+path1 = 'C:/PRANSHU/PROJECTS/Hand-Sign-Language-Classification/TESTING_DATA/Bye200.jpg'
 img_original1 = load_img(path1)
-
+imgplot = plt.imshow(img_original1)
+plt.show()
 #%%
-l=[]
-path = 'C:\PRANSHU\PROJECTS\Hand-Sign-Language-Classification/NewDataset\Train-Dataset'
-files = os.listdir(path)
-for f in files:
-    l.append(f)
-print("\nNames of Classes")
-print(l)
-#%%
-pred1 = model.predict(process_image(path1))
+pred1 = load_model.predict(process_image(path1))
 
 print(pred1)
 
 a=np.argmax(pred1,axis=1)
 print(a)
-#max_index=pred1.index(max_value)
-#print(l[max_index])
+#%%
+classes= {1: 'Bye', 2: 'CallMe', 3: 'God', 4: 'GoodLuck', 5: 'Hello', 6: 'I', 7:'ILoveYou', 8: 'No', 9: 'Ok', 10: 'Peace', 11: 'Smile', 12: 'ThumbsDown', 13: 'ThumbsUp', 14: 'Water', 15: 'Yes'}
+#print(classes)
+print("\n*****************************")
+print("Prediction: ",classes[np.argmax(pred1)+1])
+print("*****************************")
 #%%
